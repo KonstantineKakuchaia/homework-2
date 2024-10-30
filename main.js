e195d86018860929492aee1dfabbadae8692cdeb
KonstantineKakuchaia <154272100+KonstantineKakuchaia@users.noreply.github.com>
2024-10-30 13:15:15 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const users = [
+  { name: "Temo", age: 25 },
+  { name: "Lasha", age: 21 },
+  { name: "Ana", age: 29 },
+];
+
+function findYoungUser(users) {
+  let youngest = users[0];
+
+  for (let user of users) {
+    if (user.age < youngest.age) {
+      youngest = user;
+    }
+  }
+
+  return youngest.name;
+}
+
+console.log(findYoungUser(users));
+
+////////////////////////////////////////////////////////////
+
+function newObj(users) {
+  let newObjects = users.map((user) => ({ ...user }));
+  return newObjects;
+}
+
+console.log(newObj(users));
+
+///////////////////////////////////////////////////////////
+
+function roll() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function playDice() {
+  let aCount = 0;
+  let bCount = 0;
+  let aScore = 0;
+  let bScore = 0;
+
+  while (true) {
+    aScore = roll();
+    aCount++;
+    console.log(`A მომხმარებლის კამათლი: ${aScore}`);
+
+    if (aScore === 3) {
+      console.log(` A მომხმარებელმა მოიგო ${aCount} ცდაში`);
+      break;
+    }
+
+    bScore = roll();
+    bCount++;
+    console.log(`B მომხმარებლის კამათლი: ${bScore}`);
+
+    if (bScore === 3) {
+      console.log(`B მომხმარებელმა მოიგო: ${bCount} ცდაში!`);
+      break;
+    }
+  }
+}
+
+playDice();
